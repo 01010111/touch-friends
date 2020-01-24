cfe030c9ec193f5ed6daf14df8602ada57f312ee
Will Blanton <will@Wills-MacBook-Pro.local>
2020-01-24 14:00:59 -0500

    fuck sw tho

--- _resources/export/sw.js
+++ _resources/export/sw.js
@@ -13,7 +13,7 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
 	return caches.open(CACHE).then(function (cache) {
 		return cache.addAll([
-			'../',
+			'/',
 			'index.html',
 			'app.js',
 			'pixi.js',
