2261ae472bbef28339e8e0d5dc57fe93b3b1a095
Will Blanton <will@Wills-MacBook-Pro.local>
2020-01-24 14:31:07 -0500

    add icons to sw

--- _resources/export/sw.js
+++ _resources/export/sw.js
@@ -20,6 +20,15 @@ function precache() {
 			'tweenmax.js',
 			'easepack.js',
 			'manifest.json',
+			'favicon.ico',
+			'images/icons/icon-72x72.png',
+			'images/icons/icon-96x96.png',
+			'images/icons/icon-128x128.png',
+			'images/icons/icon-144x144.png',
+			'images/icons/icon-152x152.png',
+			'images/icons/icon-192x192.png',
+			'images/icons/icon-384x384.png',
+			'images/icons/icon-512x512.png',
 		]);
 	});
 }
