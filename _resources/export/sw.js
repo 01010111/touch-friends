456c49ddc5a3d7517f09c75be3509af7af1de6f6
Will Blanton <will@Wills-MacBook-Pro.local>
2020-01-24 13:43:53 -0500

    SW fix

--- _resources/export/sw.js
+++ _resources/export/sw.js
@@ -1,14 +1,31 @@
-self.addEventListener('install', function(e) {
-	e.waitUntil(
-		caches.open('airhorner').then(function(cache) {
-			return cache.addAll([
-				'/',
-				'/index.html',
-				'/app.js',
-				'/pixi.js',
-				'/tweenmax.js',
-				'/easepack.js',
-			]);
-		})
-	);
+var CACHE = 'cache-only';
+ 
+self.addEventListener('install', function(evt) {
+	console.log('The service worker is being installed.');
+	evt.waitUntil(precache());
 });
+
+self.addEventListener('fetch', function(evt) {
+	console.log('The service worker is serving the asset.');
+	evt.respondWith(fromCache(evt.request));
+});
+
+function precache() {
+	return caches.open(CACHE).then(function (cache) {
+		return cache.addAll([
+			'index.html',
+			'app.js',
+			'pixi.js',
+			'tweenmax.js',
+			'easepack.js',
+		]);
+	});
+}
+ 
+function fromCache(request) {
+	return caches.open(CACHE).then(function (cache) {
+		return cache.match(request).then(function (matching) {
+			return matching || Promise.reject('no-match');
+		});
+	});
+}
